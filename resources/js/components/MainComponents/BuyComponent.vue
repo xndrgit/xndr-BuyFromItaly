b13002d31e5e3d04d1e5c886d4c9a32f30db8869
XanderWorld <xander.mymails@gmail.com>
2023-10-31 05:33:27 +0100

    added buy box

---
+++ resources/js/components/MainComponents/BuyComponent.vue
@@ -0,0 +1,158 @@
+<template>
+
+    <div class="contact-box">
+        <div class="contact-details">
+            <img
+                :src="getImg('logo.png')"
+                alt="logo"
+                class="img-fluid logo"
+            />
+            <div class="contact-item">
+                <i class="fab fa-whatsapp"></i>
+                <div class="contact-text">
+                    <a href="https://wa.me/+393880771396">Contact us via WhatsApp</a>
+                </div>
+            </div>
+            <div class="contact-item">
+                <i class="fab fa-instagram"></i>
+                <div class="contact-text">
+                    <a href="https://www.instagram.com/xndr.ig/">@xndr.ig</a>
+                </div>
+            </div>
+            <div class="contact-item">
+                <i class="fas fa-phone-alt"></i>
+                <div class="contact-text">
+                    <a href="tel:+393880771396">+39 388 077 1396</a>
+                </div>
+            </div>
+            <div class="contact-item">
+                <i class="far fa-envelope"></i>
+                <div class="contact-text">
+                    <a href="mailto:mymails.xander@gmail.com">mymails.xander@gmail.com</a>
+                </div>
+            </div>
+            <!--                            <div class="cta-button">-->
+            <!--                                <button class="buy-button">-->
+            <!--                                    <i class="fas fa-shopping-cart"></i> Buy Now-->
+            <!--                                </button>-->
+            <!--                            </div>-->
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    mounted() {
+    },
+
+    name: "Buy",
+    components: {},
+    props: {},
+
+
+    data() {
+        return {};
+    },
+    methods: {
+        getImg(name) {
+            return `/img/${name}`;
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+.contact-box {
+    position: fixed;
+    top: 58%; /* Center vertically on the viewport */
+    left: 50%; /* Center horizontally on the viewport */
+    transform: translate(-50%, -50%); /* Center the box perfectly */
+
+    background-color: #f7f7f7;
+    border-radius: 20px;
+    padding: 30px;
+    color: #333;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+
+    border: 2px solid black;
+}
+
+.contact-details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.contact-item {
+    display: flex;
+    align-items: center;
+    margin: 15px 0;
+
+
+    cursor: pointer;
+}
+
+i {
+    font-size: 24px;
+    margin-right: 15px;
+    color: #007BFF;
+}
+
+h2 {
+    font-size: 24px;
+    margin-bottom: 10px;
+}
+
+.intro-text {
+    font-size: 16px;
+    margin-bottom: 20px;
+}
+
+.contact-text {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+}
+
+span {
+    font-weight: bold;
+    color: #007BFF;
+}
+
+.contact-info {
+    font-weight: normal;
+}
+
+cta-button {
+    margin: 30px 0;
+}
+
+.buy-button {
+    display: flex;
+    align-items: center;
+    background-color: #007BFF;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: bold;
+}
+
+.buy-button:hover {
+    background-color: #0056b3;
+}
+
+/* Add hover effect for a subtle interaction */
+.contact-item:hover {
+    transform: scale(1.05);
+    transition: transform 0.3s ease-in-out;
+}
+</style>
--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -4,9 +4,11 @@
             <div class="row">
                 <div class="col-12 d-flex flex-wrap justify-content-center">
 
-                    <Product v-for="post in postsGift" :key="post.id"
+                    <Product v-for="post in postsGift" :key="post.id" @click="showBuyChange"
                              :postGift="post"
                     />
+
+                    <BuyComponent v-if="showBuy"/>
 
                 </div>
 
@@ -17,6 +19,7 @@
 
 <script>
 import Product from "../components/MainComponents/Product.vue";
+import BuyComponent from "../components/MainComponents/BuyComponent.vue";
 
 export default {
     mounted() {
@@ -25,6 +28,7 @@ export default {
     name: "Home",
     components: {
         Product,
+        BuyComponent,
     },
     props: {
         postsGift: Array,
@@ -34,21 +38,119 @@ export default {
     data() {
         return {
             posts: [],
+            showBuy: false,
         };
     },
     methods: {
+        getImg(name) {
+            return `/img/${name}`;
+        },
+        showBuyChange() {
+
+            console.log('this.showBuy');
+            // this.showBuy = !this.showBuy;
+        },
     },
 };
 </script>
 
 <style lang="scss" scoped>
-.home{
-   padding-top: 80px;
+
+.contact-box {
+    background-color: #f7f7f7;
+    border-radius: 20px;
+    padding: 30px;
+    color: #333;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-align: center;
+
+    border: 2px solid black;
 }
-.category{
+
+.contact-details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.contact-item {
+    display: flex;
+    align-items: center;
+    margin: 15px 0;
+
+
+    cursor: pointer;
+}
+
+i {
+    font-size: 24px;
+    margin-right: 15px;
+    color: #007BFF;
+}
+
+h2 {
+    font-size: 24px;
+    margin-bottom: 10px;
+}
+
+.intro-text {
+    font-size: 16px;
+    margin-bottom: 20px;
+}
+
+.contact-text {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+}
+
+span {
+    font-weight: bold;
+    color: #007BFF;
+}
+
+.contact-info {
+    font-weight: normal;
+}
+
+cta-button {
+    margin: 30px 0;
+}
+
+.buy-button {
+    display: flex;
+    align-items: center;
+    background-color: #007BFF;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-weight: bold;
+}
+
+.buy-button:hover {
+    background-color: #0056b3;
+}
+
+/* Add hover effect for a subtle interaction */
+.contact-item:hover {
+    transform: scale(1.05);
+    transition: transform 0.3s ease-in-out;
+}
+
+.home {
+    padding-top: 80px;
+}
+
+.category {
     margin: 2.8rem 1rem;
     font-size: 1rem;
-    .link_category{
+
+    .link_category {
         text-decoration: none;
         color: black;
 
